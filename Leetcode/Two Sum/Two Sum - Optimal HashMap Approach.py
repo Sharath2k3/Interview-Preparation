f517a07badea7ce26ce4da930014217e291137fc
Sharath2k3 <122155321+Sharath2k3@users.noreply.github.com>
2024-12-03 10:43:43 +0530

    Two Sum

---
+++ Leetcode/Two Sum/Two Sum - Optimal HashMap Approach.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n=len(nums)
+        num_map = {}
+        for i in range(n):
+            num_map[nums[i]]=i
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in num_map and num_map[complement]!=i:
+                return [i,num_map[complement]]
+        return []
+
+        
