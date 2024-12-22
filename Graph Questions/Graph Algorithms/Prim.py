a2481a84db9c1a65e99177f8043242bb3f79bda1
Sharath2k3 <122155321+Sharath2k3@users.noreply.github.com>
2024-12-22 08:46:40 +0530

    Prim's Algorithm

---
+++ Graph Questions/Graph Algorithms/Prim.py
@@ -0,0 +1,32 @@
+import heapq
+
+def tree(V,E,edges):
+    adj = [[] for _ in range(V)]
+    
+    for u,v,wt in edges:
+        adj[u].append((v,wt))
+        adj[v].append((u,wt))
+
+    pq = []
+    visited = [False] * V 
+    res = 0 
+    #Sending the value of weight and u
+    heapq.heappush(pq,(0,0))
+
+    while pq:
+        wt,u = heapq.heappop(pq)
+        if visited[u]:
+            continue
+        res += wt
+        visited[u] = True
+
+        for v,weight in adj[u]:
+            if not visited[v]:
+                heapq.heappush(pq,(weight,v))
+
+    return res
+if __name__ == "__main__":
+    graph = [[0, 1, 5],
+             [1, 2, 3],
+             [0, 2, 1]]
+    print(tree(3, 3, graph))
