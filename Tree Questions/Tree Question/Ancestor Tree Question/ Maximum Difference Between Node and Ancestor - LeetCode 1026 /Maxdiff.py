2088999c7b3e3cfda2cee8aefd14419bf173bb9b
Sharath2k3 <122155321+Sharath2k3@users.noreply.github.com>
2024-12-22 11:16:20 +0530

    Added LeetCode 1026

---
+++ Tree Questions/Tree Question/Ancestor Tree Question/ Maximum Difference Between Node and Ancestor - LeetCode 1026 /Maxdiff.py
@@ -0,0 +1,28 @@
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def maxAncestorDiff(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        
+        self.diff = 0
+        self.helper(root,root.val,root.val)
+        return self.diff
+
+    def helper(self,root,min_val,max_val):
+        if root is None:
+            return 0
+        self.diff = max(self.diff , max(abs(min_val-root.val),abs(max_val-root.val)))
+        min_val = min(min_val,root.val)
+        max_val = max(max_val,root.val)
+        self.helper(root.left,min_val,max_val)
+        self.helper(root.right,min_val,max_val)
