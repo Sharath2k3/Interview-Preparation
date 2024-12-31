3fe5044f12e97c78a93f4c0393c3dbff7449ca26
Sharath2k3 <122155321+Sharath2k3@users.noreply.github.com>
2024-12-31 17:41:08 +0530

    Added Pascal Triangle II - LeetCode 119

---
+++ NeetCode/Arrays and Hashing/Pascal Triangle II - LeetCode 119/PascalTriangleII.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        res = [1]
+        prev = 1
+        for k in range(1,rowIndex + 1):
+            next_val = prev * (rowIndex-k+1) // k #FORMULA
+            res.append(next_val)
+            prev = next_val
+
+        return res
