ab4fe65ed4c5fa8c9eb94a8f378fb4b15552d1df
Sharath2k3 <122155321+Sharath2k3@users.noreply.github.com>
2024-12-31 13:57:26 +0530

    Replace Element With Greatest Element on Right Side

---
+++ NeetCode/Arrays and Hashing/Replace Elements with Greatest Element on Right Side - LeetCode 1299/Code.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        max = -1
+        i = len(arr) - 1
+        while i>=0:
+            temp = arr[i]
+            arr[i] = max 
+            if temp > max:
+                max = temp
+            i-=1
+        return arr
