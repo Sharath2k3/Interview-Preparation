12312e009da299dc3cf0632d4c7790cd78867502
Sharath2k3 <122155321+Sharath2k3@users.noreply.github.com>
2025-01-10 13:28:27 +0530

    LeetCode 374 - Guess Number Higher or Lower

---
+++ NeetCode/Binary Search/Guess Number Higher or Lower - LeetCode 374/Code.py
@@ -0,0 +1,26 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1 
+        right = n
+        while left<=right:
+            m = (left+right) //2
+            guess_res = guess(m)
+            if not guess_res:
+                return m
+            if guess_res == 1:
+                left=m+1
+            else:
+                right=m-1
+    
+        return -1
